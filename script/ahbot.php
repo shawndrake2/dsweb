bea3bfabf085f429d23343a19ad78b98b2840529
Drake <shawndrake2@gmail.com>
2018-08-23 23:30:12 -0400

    update ahBot documentation

--- script/ahbot.php
+++ script/ahbot.php
@@ -6,16 +6,18 @@ require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 /**
  *
- * I will be running this script every 19-22 minutes in order to simulate
+ * I will be running this script every 30 minutes in order to simulate
  * a real economy. (arbitrary numbers based on perceived activity)
  *
- * This script will handle cleanup of old Bot listings so that
- * the server does not have to, cleaning up the bot's delivery box,
- * and stocking anywhere from 0 to 100 random items each time it runs.
+ * This script will do the following:
+ *   - handle cleanup of old Bot listings so that the server does not have to
+ *   - cleaning up the bot's delivery box
+ *   - stocking anywhere from 0 to 100 random items each time it runs
+ *   - buying random items within the treasury budget
  *
  * CRONTAB ENTRY:
  * logfile=/tmp/ahbot.log
- * *\/19 * * * * sleep $(( RANDOM \% 180 )); cd /path/to/dsweb/script && php ahbot.php >> $logfile 2>&1
+ * *\/30 * * * * cd /path/to/dsweb/script && php ahbot.php >> $logfile 2>&1
  *
 */
 
