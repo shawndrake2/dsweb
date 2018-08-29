f2604c7232a488a805bbd5ee99ea9d1c141d615c
Drake <shawndrake2@gmail.com>
2018-08-28 21:10:21 -0400

    Added Webpack build

---
+++ build/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+const getJsPath = () => {
+  return path.resolve(__dirname, '../', 'public', 'assets', 'js')
+}
+
+module.exports = {
+  mode: 'development',
+  entry: `${getJsPath()}/main.js`,
+  output: {
+    path: path.resolve(getJsPath(), 'dist'),
+    filename: 'main.bundle.js'
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  }
+}
